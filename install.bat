d2cc134e43285cbcb20bb21bcc363b91d73e2f6f
TopSpeed0 <107992933+TopSpeed0@users.noreply.github.com>
2023-06-28 14:18:32 +0300

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
-# Copy new Profiles
+echo "Start Copy Profiles"
 del "C:\ProgramData\Cisco\Cisco Secure Client\VPN\Profile\*.xml"
 echo del "C:\ProgramData\Cisco\Cisco Secure Client\Umbrella\OrgInfo.json"
 copy "%~dp0\Profiles\vpn\CognyteAnyConnectProfile.xml" "C:\ProgramData\Cisco\Cisco Secure Client\VPN\Profile"
 copy "%~dp0\Profiles\umbrella\OrgInfo.json" "C:\ProgramData\Cisco\Cisco Secure Client\Umbrella"
 pause
-PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0\install.ps1"
+PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0install.ps1"
+echo "End Installation"
+pause
--- install.ps1
+++ install.ps1
@@ -1,5 +1,28 @@
 # install C:\Users\ybohadana\OneDrive - COGNYTE\Desktop\Cisco AnyConnect 5.0.3
 $CAC = Get-ChildItem (Get-Location)*.msi*
+$u_CAC = $CAC | Sort-Object -Descending
+# Uninstalltion
+foreach ($msiPath in $u_CAC) {
+    # Start the Uninstalltion process
+    if ($msiPath -match '-nam-predeploy-k9') {
+        $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x `"$msiPath`" REBOOT=ReallySuppress /qn /norestart" -PassThru -Wait
+    } else {
+        $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x `"$msiPath`" /qn /norestart" -PassThru -Wait
+    }
+    # Check the exit code of the Uninstalltion process
+    if ($process.ExitCode -eq 0) {
+        Write-Host "MSI:$msiPath Uninstalltion completed successfully."
+    }
+    else {
+        Write-Host "MSI:$msiPath Uninstalltion failed with exit code $($process.ExitCode)." -ForegroundColor Red
+        pause
+    }
+}
+# Remove-Item 'C:\ProgramData\Cisco\Cisco Secure Client\Umbrella' -Force -Confirm:$true -ErrorAction SilentlyContinue
+Write-Host "Uninstalltion END."
+# end of Uninstalltion
+
+# installation
 foreach ($msiPath in $CAC) {
     # Start
     Write-Host "Start MSI installation of:$msiPath." -ForegroundColor Yellow
@@ -8,6 +31,7 @@ foreach ($msiPath in $CAC) {
         $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$msiPath`" REBOOT=ReallySuppress /qn /norestart" -PassThru -Wait
     } elseif ($msiPath -notmatch 'iseposture-predeploy-k9') {
         $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$msiPath`" /qn /norestart" -PassThru -Wait
+    } else {
     }
     # Check the exit code of the installation process
     if ($process.ExitCode -eq 0) {
@@ -15,7 +39,9 @@ foreach ($msiPath in $CAC) {
     }
     else {
         Write-Host "MSI:$msiPath installation failed with exit code $($process.ExitCode)." -ForegroundColor Red
+        pause
     }
 }
+# end of installation
 Write-Host "installation END."
 Start-Process -FilePath "C:\Program Files (x86)\Cisco\Cisco Secure Client\UI\csc_ui.exe"
