6b170fbc7c4121c30f93ed6f61eeba8ec45d8d4e
TopSpeed0 <107992933+TopSpeed0@users.noreply.github.com>
2023-06-28 14:29:32 +0300

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,27 +1,3 @@
-# install C:\Users\ybohadana\OneDrive - COGNYTE\Desktop\Cisco AnyConnect 5.0.3
-$CAC = Get-ChildItem (Get-Location)*.msi*
-$u_CAC = $CAC | Sort-Object -Descending
-# Uninstalltion
-foreach ($msiPath in $u_CAC) {
-    # Start the Uninstalltion process
-    if ($msiPath -match '-nam-predeploy-k9') {
-        $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x `"$msiPath`" REBOOT=ReallySuppress /qn /norestart" -PassThru -Wait
-    } else {
-        $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x `"$msiPath`" /qn /norestart" -PassThru -Wait
-    }
-    # Check the exit code of the Uninstalltion process
-    if ($process.ExitCode -eq 0) {
-        Write-Host "MSI:$msiPath Uninstalltion completed successfully."
-    }
-    else {
-        Write-Host "MSI:$msiPath Uninstalltion failed with exit code $($process.ExitCode)." -ForegroundColor Red
-        pause
-    }
-}
-# Remove-Item 'C:\ProgramData\Cisco\Cisco Secure Client\Umbrella' -Force -Confirm:$true -ErrorAction SilentlyContinue
-Write-Host "Uninstalltion END."
-# end of Uninstalltion
-
 # installation
 foreach ($msiPath in $CAC) {
     # Start
