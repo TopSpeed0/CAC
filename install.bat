17e7cfc1eeffbe4cbff7e309e2e28e0444dba952
TopSpeed0 <107992933+TopSpeed0@users.noreply.github.com>
2023-06-28 14:50:08 +0300

    Add files via upload

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@ del "C:\ProgramData\Cisco\Cisco Secure Client\VPN\Profile\*.xml"
 echo del "C:\ProgramData\Cisco\Cisco Secure Client\Umbrella\OrgInfo.json"
 copy "%~dp0\Profiles\vpn\CognyteAnyConnectProfile.xml" "C:\ProgramData\Cisco\Cisco Secure Client\VPN\Profile"
 copy "%~dp0\Profiles\umbrella\OrgInfo.json" "C:\ProgramData\Cisco\Cisco Secure Client\Umbrella"
+copy "%~dp0\Profiles\System\*" "C:\ProgramData\Cisco\Cisco Secure Client\Network Access Manager\system"
 pause
 PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0install.ps1"
 echo "End Installation"
